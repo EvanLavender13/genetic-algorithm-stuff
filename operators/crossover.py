ef535f2161a65572dfe3111456cf6b0350baf8cc
Evan Lavender <evanlavender@pop-os.localdomain>
2019-09-09 21:40:49 -0400

    Maybe I'll do unit tests..

--- operators/crossover.py
+++ operators/crossover.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def single_point(ind1, ind2):
+def single_point(ind1, ind2, p=None):
     size = len(ind1)
-    point = np.random.randint(1, size - 1)
+    point = p if 0 < p < size else np.random.randint(1, size - 1)
 
-    return np.concatenate([ind1[:point], ind2[point:]])
+    return list(np.concatenate([ind1[:point], ind2[point:]]))
--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -1,9 +1,22 @@
 import unittest
 
+from operators import crossover
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class TestCrossover(unittest.TestCase):
+
+    def test_single_point(self):
+        ind1 = [1, 1, 1, 1, 1]
+        ind2 = [0, 0, 0, 0, 0]
+
+        child = crossover.single_point(ind1, ind2, p=1)
+        self.assertEqual(child, [1, 0, 0, 0, 0])
+        child = crossover.single_point(ind1, ind2, p=2)
+        self.assertEqual(child, [1, 1, 0, 0, 0])
+        child = crossover.single_point(ind1, ind2, p=3)
+        self.assertEqual(child, [1, 1, 1, 0, 0])
+        child = crossover.single_point(ind1, ind2, p=4)
+        self.assertEqual(child, [1, 1, 1, 1, 0])
 
 
 if __name__ == '__main__':
