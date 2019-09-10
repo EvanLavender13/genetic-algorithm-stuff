ce3e0276db13bfbe2fd6450be0602f4789d0db88
Evan Lavender <evanlavender@pop-os.localdomain>
2019-09-09 21:46:11 -0400

    Added test for that bug

--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -18,6 +18,13 @@ class TestCrossover(unittest.TestCase):
         child = crossover.single_point(ind1, ind2, p=4)
         self.assertEqual(child, [1, 1, 1, 1, 0])
 
+        child = crossover.single_point(ind1, ind2, p=0)
+        self.assertEqual(len(child), len(ind1))
+        child = crossover.single_point(ind1, ind2, p=5)
+        self.assertEqual(len(child), len(ind1))
+        child = crossover.single_point(ind1, ind2)
+        self.assertEqual(len(child), len(ind1))
+
 
 if __name__ == '__main__':
     unittest.main()
